98d9f772fe178d95974dd7a605fbb3231cda43b1
Zoe Keller <keller.zoe98@gmail.com>
2020-06-10 14:03:30 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog 
+  def dog_name(current_name)
+    @name= current_name
+  end 
+  
+  def dog_name 
+    @name
+  end 
+  
+  def dog_breed(type)
+    @breed= type 
+  end 
+  
+  def dog_breed 
+    @breed 
+  end 
+  
+end 
